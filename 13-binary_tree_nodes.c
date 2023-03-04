56eae8662225f02c84c8b7188c2eddf71517bbda
dhev-wisdom <nonsowisdom62@gmail.com>
2023-03-03 18:15:26 -0800

    Check

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,23 +1,53 @@
 #include "binary_trees.h"
 
+int sum = 0;
+
 /**
- * binary_tree_nodes - function that counts the nodes.
- * with at least 1 child in a binary tree
- * @tree: pointer to the root node of the tree to count the number of nodes
- * Return: 0 if @tree is NULL, otherwise number of nodes
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * @tree: a pointer to the root node of the tree to count the number of nodes
+ *
+ * Return: the number of nodes,
+ * or 0, if tree is NULL
  */
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
+	int res = 0;
 
-	size_t nodes = 0;
+	if (tree)
+		tree_preorder(tree, &nodes_count);
+	res = sum;
+	sum = 0;
+	return (res);
+}
 
-	if (tree->left != NULL || tree->right != NULL)
-		nodes += 1;
-	nodes += binary_tree_nodes(tree->left);
-	nodes += binary_tree_nodes(tree->right);
+/**
+ * nodes_count - counts the nodes in a binary tree
+ * @node: a pointer to the root node of the tree
+ *
+ * Return: void
+ */
 
-	return (nodes);
+void nodes_count(const binary_tree_t *node)
+{
+	if (node && (node->left || node->right))
+		sum++;
+}
+
+/**
+ * tree_preorder - goes through a binary tree using pre-order traversal
+ * @tree: a pointer to the root node of the tree to traverse
+ * @func: a pointer to a function to call for each node
+ *
+ * Return: void
+ */
+void tree_preorder(const binary_tree_t *tree,
+	void (*func)(const binary_tree_t *))
+{
+	if (tree)
+	{
+		func(tree);
+		tree_preorder(tree->left, func);
+		tree_preorder(tree->right, func);
+	}
 }
--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,34 +1,34 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_full - Checks id a binary tree is full.
- * @tree: pointer to node of tree to be checked
- * Return: 0 if tree is NULL or tree is not full, otherwise 1
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: a pointer to the root node of the tree to check
+ *
+ * Return: 1, if tree is full,
+ * otherwise, 0, if not full, or tree is NULL
  */
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
-	return (is_full_recursive(tree));
+	return (full_tree(tree));
 }
 
 /**
- * is_full_recursive - Checks if a binary tree is full recursively.
- * @tree: pointer to root node of tree to check.
+ * full_tree - checks if a binary tree is full
+ * @tree: a pointer to the root node of the tree to check
  *
- * Return: 0 if tree is not full or tree doesn't exist. Otherwise, 1.
+ * Return: 1, if tree is full,
+ * otherwise, 0, if not full, or tree is NULL
  */
 
-int is_full_recursive(const binary_tree_t *tree)
+int full_tree(const binary_tree_t *tree)
 {
-	if (tree)
-	{
-		if ((tree->left != NULL && tree->right == NULL) ||
-		    (tree->left == NULL && tree->right != NULL) ||
-		     is_full_recursive(tree->left) == 0 ||
-		      is_full_recursive(tree->right) == 0)
-			return (0);
-	}
-	return (1);
+	if (!tree)
+		return (0);
+	if ((!tree->left && !tree->right))
+		return (1);
+	if (tree->left && tree->right)
+		return (full_tree(tree->left) && full_tree(tree->right));
+	return (0);
 }
+
