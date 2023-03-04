21d446f50b4515657e6a1c6a18d79b6c6fc5ae8c
dhev-wisdom <nonsowisdom62@gmail.com>
2023-03-03 17:24:04 -0800

    task-12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,7 +12,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	{
 		if (tree->left == NULL && tree->right == NULL)
 			return (1);
-		return (1 + binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
-	}						        }
+		return (1 + binary_tree_leaves(tree->left) +
+			binary_tree_leaves(tree->right));
 	return (0);
 }
