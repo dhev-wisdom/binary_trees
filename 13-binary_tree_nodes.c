9c255ddd8a42d963844e10c15e41c5fbfbfeabc7
dhev-wisdom <nonsowisdom62@gmail.com>
2023-03-09 15:27:48 -0800

    task-13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,42 +12,14 @@ int sum = 0;
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	int res = 0;
+	size_t nodes = 0;
 
-	if (tree)
-		tree_preorder(tree, &nodes_count);
-	res = sum;
-	sum = 0;
-	return (res);
-}
-
-/**
- * nodes_count - counts the nodes in a binary tree
- * @node: a pointer to the root node of the tree
- *
- * Return: void
- */
-
-void nodes_count(const binary_tree_t *node)
-{
-	if (node && (node->left || node->right))
-		sum++;
-}
-
-/**
- * tree_preorder - goes through a binary tree using pre-order traversal
- * @tree: a pointer to the root node of the tree to traverse
- * @func: a pointer to a function to call for each node
- *
- * Return: void
- */
-void tree_preorder(const binary_tree_t *tree,
-	void (*func)(const binary_tree_t *))
-{
 	if (tree)
 	{
-		func(tree);
-		tree_preorder(tree->left, func);
-		tree_preorder(tree->right, func);
+		if (tree->left || tree->right)
+			nodes++;
+		nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->right);
 	}
+	return (nodes);
 }
